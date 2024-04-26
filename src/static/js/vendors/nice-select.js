dbdef4e37ef037351ec7005bf3b5c49ad4a9a632
ElifYolacan <elif.yolacan@alpata.com>
2024-04-26 14:46:16 +0300

    enterpad renk düzenleme yapıldı

--- src/static/js/vendors/nice-select.js
+++ src/static/js/vendors/nice-select.js
@@ -63,7 +63,7 @@
         .addClass($select.attr('class') || '')
         .addClass($select.attr('disabled') ? 'disabled' : '')
         .attr('tabindex', $select.attr('disabled') ? null : '0')
-        .html('<span class="current"></span><ul class="list thin-scrollbar"></ul>')
+        .html('<span class="current"></span><ul class="list thin-scrollbar" style="overflow-x: hidden; width: 230px;"></ul>')
       );
 
       var $dropdown = $select.next();
